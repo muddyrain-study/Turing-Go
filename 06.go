c191b6a4f11b1b108a8cdc8a9d6c20038d4b45c5
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-02-08 15:44:41 +0800

    slice question

--- 06.go
+++ 06.go
@@ -3,33 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	//// 声明一个二维整型数组，两个维度的长度分别是 4 和 2
-	//var array [4][2]int
-	//// 使用数组字面量来声明并初始化一个二维整型数组
-	//array = [4][2]int{{10, 11}, {20, 21}, {30, 31}, {40, 41}}
-	//// 声明并初始化数组中索引为 1 和 3 的元素
-	//array = [4][2]int{1: {20, 21}, 3: {40, 41}}
-	//// 声明并初始化数组中指定的元素
-	//array = [4][2]int{1: {0: 20}, 3: {1: 41}}
-	//
-	//fmt.Println(array[1][0])
-	//
-	//for index, value := range array {
-	//	fmt.Printf("索引:%d,值:%d \n", index, value)
-	//}
-
-	// 声明两个二维整型数组 [2]int [2]int
-	var array1 [2][2]int
-	var array2 [2][2]int
-	// 为array2的每个元素赋值
-	array2[0][0] = 10
-	array2[0][1] = 20
-	array2[1][0] = 30
-	array2[1][1] = 40
-	// 将 array2 的值复制给 array1
-	array1 = array2
-
-	fmt.Println(array1)
-	fmt.Println(array2)
-
+	var numbers4 = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	myslice := numbers4[4:6]
+	//这打印出来长度为2
+	fmt.Printf("myslice为 %d, 其长度为: %d\n", myslice, len(myslice))
+	myslice = myslice[:cap(myslice)]
+	//为什么 myslice 的长度为2，却能访问到第四个元素
+	fmt.Printf("myslice的第四个元素为: %d", myslice[3])
 }
