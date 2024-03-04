98b50ec814654c0bfd5420aa391ccd0ad07cb9bf
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-03-04 18:26:38 +0800

    feat: 06 goto

--- 06.go
+++ 06.go
@@ -1,54 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
 
 func main() {
-	/* 定义局部变量 */
-	var grade string = "B"
-	var score int = 90
-
-	switch score {
-	case 90:
-		grade = "A"
-	case 80:
-		grade = "B"
-	case 50, 60, 70:
-		grade = "C"
-	default:
-		grade = "D"
+	err := firstCheckError()
+	if err != nil {
+		fmt.Println(err)
+		goto onExit
 	}
-	//swtich后面如果没有条件表达式，则会对true进行匹配
-	//swtich后面如果没有条件表达式，则会对true进行匹配
-	switch {
-	case grade == "A":
-		fmt.Printf("优秀!\n")
-	case grade == "B", grade == "C":
-		fmt.Printf("良好\n")
-	case grade == "D":
-		fmt.Printf("及格\n")
-	case grade == "F":
-		fmt.Printf("不及格\n")
-	default:
-		fmt.Printf("差\n")
+	err = secondCheckError()
+	if err != nil {
+		fmt.Println(err)
+		goto onExit
 	}
-	fmt.Printf("你的等级是 %s\n", grade)
+	fmt.Println("done")
+	return
+onExit:
+	exitProcess()
+}
 
-	//var s = "hello"
-	//switch {
-	//case s == "hello":
-	//	fmt.Println("hello")
-	//	fallthrough
-	//
-	//case s != "world":
-	//	fmt.Println("world")
-	//}
+func secondCheckError() interface{} {
+	return errors.New("错误2")
+}
 
-	var s = "hello"
-	switch {
-	case s == "hello":
-		fmt.Println("hello")
-		fallthrough
-	case s == "world":
-		fmt.Println("world")
-	}
+func exitProcess() {
+	fmt.Println("exit")
+	//退出
+	os.Exit(1)
+}
+
+func firstCheckError() interface{} {
+	return errors.New("错误1")
 }
