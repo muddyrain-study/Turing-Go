15b718d9b1715bb5b8cb388d5805c2d8e2b77730
muddyrain <qiu3291002845@gmail.com>
2024-05-04 21:31:00 +0800

    feat: 07 闭包

--- 07.go
+++ 07.go
@@ -1,15 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
+func playerGen(name string) func() (string, int) {
+	// 血量一直为150
+	hp := 150
+	return func() (string, int) {
+		return name, hp
+	}
+}
+func playerGen1() func(string) (string, int) {
+	// 血量一直为150
+	hp := 150
+	return func(name string) (string, int) {
+		return name, hp
+	}
+}
 func main() {
-	res := func(x float64) float64 {
-		sqrt := math.Sqrt(x)
-		fmt.Println(sqrt)
-		return sqrt
-	}(9)
-	fmt.Println(res)
+	generator := playerGen1()
+	name1, hp1 := generator("玩家1")
+	name2, hp2 := generator("玩家2")
+	name3, hp3 := generator("玩家3")
+	println(name1, hp1)
+	println(name2, hp2)
+	println(name3, hp3)
 }
