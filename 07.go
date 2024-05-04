3e9719617c4273638db28db3092a28645ce57f47
muddyrain <qiu3291002845@gmail.com>
2024-05-04 21:18:54 +0800

    feat: 07

--- 07.go
+++ 07.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-func test(s string, n ...int) string {
-	var x int
-	for _, i := range n {
-		x += i
-	}
-
-	return fmt.Sprintf(s, x)
-}
-
 func main() {
-	s := []int{1, 2, 3}
-	res := test("sum: %d", s...) // slice... 展开slice
-	println(res)
+	res := func(x float64) float64 {
+		sqrt := math.Sqrt(x)
+		fmt.Println(sqrt)
+		return sqrt
+	}(9)
+	fmt.Println(res)
 }
