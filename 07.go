85f9df92f53e63e771f6b1f846d6c1b5713586f2
muddyrain <qiu3291002845@gmail.com>
2024-05-05 15:43:28 +0800

    feat: 07 defer

--- 07.go
+++ 07.go
@@ -1,25 +1,11 @@
 package main
 
-func playerGen(name string) func() (string, int) {
-	// 血量一直为150
-	hp := 150
-	return func() (string, int) {
-		return name, hp
-	}
-}
-func playerGen1() func(string) (string, int) {
-	// 血量一直为150
-	hp := 150
-	return func(name string) (string, int) {
-		return name, hp
-	}
-}
 func main() {
-	generator := playerGen1()
-	name1, hp1 := generator("玩家1")
-	name2, hp2 := generator("玩家2")
-	name3, hp3 := generator("玩家3")
-	println(name1, hp1)
-	println(name2, hp2)
-	println(name3, hp3)
+	var whatever = [5]int{1, 2, 3, 4, 5}
+	for i := 0; i < len(whatever); i++ {
+		i := i
+		defer func() {
+			println(i)
+		}()
+	}
 }
