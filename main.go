bc089ade5ecf9f37f199dc562a64fb1055fb3a3c
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-11-19 17:38:29 +0800

    feat: lock

--- main.go
+++ main.go
@@ -1,26 +1,50 @@
 package main
 
-func counter(out chan<- int) {
-	for i := 0; i < 100; i++ {
-		out <- i
-	}
-	close(out)
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	x      int64
+	wg     sync.WaitGroup
+	lock   sync.Mutex
+	rwlock sync.RWMutex
+)
+
+func write() {
+	//lock.Lock() // 加互斥锁
+	rwlock.Lock() // 加写锁
+	x = x + 1
+	time.Sleep(10 * time.Millisecond) // 假设写操作耗时10毫秒
+	rwlock.Unlock()                   // 解写锁
+	//lock.Unlock() // 解互斥锁
+	wg.Done()
 }
-func squarer(out chan<- int, in <-chan int) {
-	for i := range in {
-		out <- i * i
-	}
-	close(out)
+
+func read() {
+	//lock.Lock() // 加互斥锁
+	rwlock.RLock()               // 加读锁
+	time.Sleep(time.Millisecond) // 假设读操作耗时1毫秒
+	rwlock.RUnlock()             // 解读锁
+	//lock.Unlock() // 解互斥锁
+	wg.Done()
 }
-func printer(in <-chan int) {
-	for i := range in {
-		println(i)
-	}
-}
+
 func main() {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
-	go counter(ch1)
-	go squarer(ch2, ch1)
-	printer(ch2)
+	start := time.Now()
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go write()
+	}
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go read()
+	}
+
+	wg.Wait()
+	end := time.Now()
+	fmt.Println(end.Sub(start))
 }
