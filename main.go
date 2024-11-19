894c6e3f75c9ce691fb669e8392d4b4bfe40b959
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-11-19 10:00:04 +0800

    feat: reader

--- main.go
+++ main.go
@@ -2,18 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	// 实例化玩家对象，并设速度为0.5
-	p := NewPlayer(0.5)
-	// 让玩家移动到3,1点
-	p.MoveTo(Vec2{3, 1})
-	// 如果没有到达就一直循环
-	for !p.IsArrived() {
-		// 更新玩家位置
-		p.Update()
-		// 打印每次移动后的玩家位置
-		fmt.Println(p.Pos())
+	file, err := os.Open("./main.go")
+	if err != nil {
+		panic(err)
 	}
+	defer file.Close()
+	var content []byte
+	for {
+		buf := make([]byte, 128)
+		n, err := file.Read(buf[:])
+		if err == io.EOF {
+			// 读取到文件末尾
+			break
+		}
+		if err != nil {
+			fmt.Println("read file err", err)
+			return
+		}
+		content = append(content, buf[:n]...)
+	}
+	fmt.Println(string(content))
 }
