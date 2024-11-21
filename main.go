29efd6652da666015fb764a554a960a8a669f525
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-11-21 10:59:14 +0800

    feat: db insert

--- main.go
+++ main.go
@@ -1,43 +1,53 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"net"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"time"
 )
 
-var wrapperPath = "/Users/qiushunmeng/Movies/GoLang/课程资料/"
+var DB *sql.DB
 
-func process(conn net.Conn) {
-	defer conn.Close() // 关闭连接
-	for {
-		reader := bufio.NewReader(conn)
-		var buf [128]byte
-		n, err := reader.Read(buf[:])
-		if err != nil {
-			fmt.Println("read from client failed, err:", err)
-			break
-		}
-		receiveStr := string(buf[:n])
-		fmt.Println("收到client端发来的数据：", receiveStr)
-		// 读取文件内容
-		conn.Write([]byte(receiveStr))
-	}
-}
-func main() {
-	listen, err := net.Listen("tcp", "127.0.0.1:20000")
+func init() {
+	// 执行 main 之前，会先执行 init
+	db, err := sql.Open("mysql", "root:mm001030@tcp(127.0.0.1:3306)/go_test")
 	if err != nil {
-		fmt.Println("listen failed, err:", err)
+		log.Println("open database error:", err)
+		panic(err)
+	}
+	// 设置最大连接数
+	db.SetMaxIdleConns(5)
+	// 设置最大打开连接数
+	db.SetMaxOpenConns(100)
+	// 设置连接的最大存活时间
+	db.SetConnMaxLifetime(time.Minute * 3)
+	// 设置连接的最大空闲时间
+	db.SetConnMaxIdleTime(time.Minute * 1)
+
+	err = db.Ping()
+	if err != nil {
+		log.Println("open database error:", err)
+		db.Close()
+		panic(err)
+	}
+	DB = db
+
+	log.Println("open database success")
+}
+
+func save(username string, sex string, email string) {
+	res, err := DB.Exec("insert into user (username,sex,email) values (?,?,?)", username, sex, email)
+	if err != nil {
+		log.Println("insert data error:", err)
 		return
 	}
-	for {
-		conn, err := listen.Accept()
-		if err != nil {
-			fmt.Println("accept failed, err:", err)
-			continue
-		}
-		go process(conn)
+	id, _ := res.LastInsertId()
+	log.Println("insert success, id:", id)
+}
 
-	}
+func main() {
+	defer DB.Close()
 
+	save("test", "男", "123@qq.com")
 }
