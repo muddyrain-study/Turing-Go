9e8771f39cbf6f03a3adfc5e86a133e5a786c28b
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-11-25 16:07:37 +0800

    feat: json

--- main.go
+++ main.go
@@ -1,39 +1,27 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
+	"encoding/json"
+	"os"
 )
 
+type Person struct {
+	Name   string   `json:"name"`
+	Age    int      `json:"age"`
+	Email  string   `json:"email"`
+	Parent []string `json:"parent"`
+}
+
 func main() {
-	data := "123456789"
-	//通过[]byte创建Reader
-	re := bytes.NewReader([]byte(data))
-
-	buf := make([]byte, 2)
-
-	re.Seek(0, 0)
-	//设置偏移量
-	for {
-		//一个字节一个字节的读
-		b, err := re.ReadByte()
-		if err != nil {
-			break
-		}
-		fmt.Println(string(b))
+	p := Person{
+		Name:   "zhangsan",
+		Age:    20,
+		Email:  "zhangsan@mail.com",
+		Parent: []string{"Daddy", "Mom"},
 	}
-	fmt.Println("----------------")
+	f, _ := os.OpenFile("test.json", os.O_WRONLY, 077)
+	defer f.Close()
 
-	re.Seek(0, 0)
-	off := int64(0)
-	for {
-		//指定偏移量读取
-		n, err := re.ReadAt(buf, off)
-		if err != nil {
-			break
-		}
-		off += int64(n)
-		fmt.Println(off, string(buf[:n]))
-	}
-
+	d := json.NewEncoder(f)
+	d.Encode(p)
 }
