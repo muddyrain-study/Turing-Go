d437d8bd1341696e2ca6cb51113758df222ac83d
Sophia Rodriguez <66107882+muddyrain@users.noreply.github.com>
2024-12-05 15:16:51 +0800

    feat: tag optimize

--- server/game/logic/roleAttribute.go
+++ server/game/logic/roleAttribute.go
@@ -9,11 +9,16 @@ import (
 	"Turing-Go/server/game/model/data"
 	"encoding/json"
 	"log"
+	"sync"
 )
 
-var RoleAttrService = &roleAttrService{}
+var RoleAttrService = &roleAttrService{
+	attrs: make(map[int]*data.RoleAttribute),
+}
 
 type roleAttrService struct {
+	attrs map[int]*data.RoleAttribute
+	mutex sync.RWMutex
 }
 
 func (r *roleAttrService) TryCreate(rid int, conn net.WSConn) error {
@@ -27,22 +32,30 @@ func (r *roleAttrService) TryCreate(rid int, conn net.WSConn) error {
 		roleAttribute.RId = rid
 		roleAttribute.UnionId = 0
 		roleAttribute.ParentId = 0
+		roleAttribute.PosTags = ""
 		_, err := db.Engine.Table(roleAttribute).Insert(&roleAttribute)
 		if err != nil {
 			log.Println("插入角色属性异常", err)
 			return common.New(constant.DBError, "数据库错误")
 		}
 	}
+	r.mutex.Lock()
+	r.attrs[rid] = &roleAttribute
+	r.mutex.Unlock()
 	return nil
 
 }
 
 func (r *roleAttrService) GetTagList(rid int) ([]model.PosTag, error) {
-	roleAttribute := &data.RoleAttribute{}
-	ok, err := db.Engine.Table(roleAttribute).Where("rid=?", rid).Get(roleAttribute)
-	if err != nil {
-		log.Println("查询角色属性异常", err)
-		return nil, common.New(constant.DBError, "数据库错误")
+	var err error
+	roleAttribute, ok := r.attrs[rid]
+	if !ok {
+		roleAttribute = &data.RoleAttribute{}
+		ok, err = db.Engine.Table(roleAttribute).Where("rid=?", rid).Get(roleAttribute)
+		if err != nil {
+			log.Println("查询角色属性异常", err)
+			return nil, common.New(constant.DBError, "数据库错误")
+		}
 	}
 	posTags := make([]model.PosTag, 0)
 	if !ok {
